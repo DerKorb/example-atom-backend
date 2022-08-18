78f4a166406fb63fe3bd8308043f77605eba4133
Korbinian Sollner <ksollner@feinarbyte.de>
2022-08-18 10:16:07 +0200

    somehow fix subscriptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,58 +6,19 @@ import { GraphQLModule } from '@nestjs/graphql/dist/graphql.module';
 import { AtomResolver } from '@feinarbyte/atom-module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ChatConfig } from './examples/chat.config';
-import { Context } from 'apollo-server-core';
-import { GqlModuleOptions } from '@nestjs/graphql';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
     AtomModule.forRoot({
       projectConfig: ChatConfig, // <-- 1. go here
     }),
-    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+    EventEmitterModule.forRoot({ global: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      useFactory: async (): Promise<Omit<ApolloDriverConfig, 'driver'>> => ({
-        autoSchemaFile: true,
-        playground: true,
-        installSubscriptionHandlers: true,
-        subscriptions: {
-          'subscriptions-transport-ws': true,
-          'graphql-ws': {
-            // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-            onConnect: (context: Context<any>) => {
-              const { connectionParams, extra } = context;
-              const token = connectionParams?.token;
-              console.log(1);
-              throw new Error('123');
-              // returning false here will cause urql to try to connect again
-
-              // this mocks the headers into the right place in the context
-              // so none of the rest of our code has to change
-              (extra as any).headers = { authorization: `Bearer ${token}` };
-            },
-          },
-        },
-        cors: {
-          credentials: true,
-          methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-          origin: '*',
-        },
-        context: ({ extra, req }) => {
-          if (extra?.request) {
-            return {
-              req: {
-                ...extra?.request,
-                headers: {
-                  ...extra?.request?.headers,
-                  ...extra?.headers,
-                },
-              },
-            };
-          }
-
-          return { req: req };
-        },
-      }),
+      autoSchemaFile: true,
+      playground: true,
+      installSubscriptionHandlers: true,
     }),
   ],
   controllers: [AppController],
